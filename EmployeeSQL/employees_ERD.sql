9dfc4eee9cf37fa982967a05783c287ac3796930
terry.schoch@gmail.com <terryschoch@Terrys-Mac-mini.local>
2023-01-02 22:54:07 -0500

    Cleaned up .sql code, organized files and updated ReadMe

--- EmployeeSQL/employees_ERD.sql
+++ EmployeeSQL/employees_ERD.sql
@@ -4,8 +4,8 @@
 
 
 CREATE TABLE "Job Titles" (
-    "title_id" VARCHAR(255)   NOT NULL,
-    "title" VARCHAR(255)   NOT NULL,
+    "title_id" VARCHAR(10)   NOT NULL,
+    "title" VARCHAR(35)   NOT NULL,
     CONSTRAINT "pk_Job Titles" PRIMARY KEY (
         "title_id"
      )
@@ -13,11 +13,11 @@ CREATE TABLE "Job Titles" (
 
 CREATE TABLE "Employees" (
     "emp_no" INTEGER   NOT NULL,
-    "emp_title_id" VARCHAR(255)   NOT NULL,
+    "emp_title_id" VARCHAR(35)   NOT NULL,
     "birth_date" DATE   NOT NULL,
-    "first_name" VARCHAR(255)   NOT NULL,
-    "last_name" VARCHAR(255)   NOT NULL,
-    "sex" VARCHAR(255)   NOT NULL,
+    "first_name" VARCHAR(35)   NOT NULL,
+    "last_name" VARCHAR(35)   NOT NULL,
+    "sex" VARCHAR(35)   NOT NULL,
     "hire_date" DATE   NOT NULL,
     CONSTRAINT "pk_Employees" PRIMARY KEY (
         "emp_no"
@@ -25,8 +25,8 @@ CREATE TABLE "Employees" (
 );
 
 CREATE TABLE "Departments" (
-    "dept_no" VARCHAR(255)   NOT NULL,
-    "dept_name" VARCHAR(255)   NOT NULL,
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "dept_name" VARCHAR(35)   NOT NULL,
     CONSTRAINT "pk_Departments" PRIMARY KEY (
         "dept_no"
      )
@@ -34,12 +34,12 @@ CREATE TABLE "Departments" (
 
 CREATE TABLE "Dept Employees" (
     "emp_no" INTEGER   NOT NULL,
-    "dept_no" VARCHAR(255)   NOT NULL
+    "dept_no" VARCHAR(10)   NOT NULL
 );
 
 CREATE TABLE "Dept Managers" (
-    "emp_no" INTEGER   NOT NULL,
-    "dept_no" VARCHAR(255)   NOT NULL
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "emp_no" INTEGER   NOT NULL
 );
 
 CREATE TABLE "Salaries" (
@@ -59,11 +59,11 @@ REFERENCES "Employees" ("emp_no");
 ALTER TABLE "Dept Employees" ADD CONSTRAINT "fk_Dept Employees_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "Departments" ("dept_no");
 
-ALTER TABLE "Dept Managers" ADD CONSTRAINT "fk_Dept Managers_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
-
 ALTER TABLE "Dept Managers" ADD CONSTRAINT "fk_Dept Managers_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept Managers" ADD CONSTRAINT "fk_Dept Managers_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
 
 ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "Employees" ("emp_no");
--- EmployeeSQL/employees_table_schemata.sql
+++ EmployeeSQL/employees_table_schemata.sql
@@ -8,38 +8,38 @@ DROP TABLE IF EXISTS salaries;
 
 -- Create new tables to import data
 CREATE TABLE job_titles (
-	title_id VARCHAR(255) NOT NULL,
-	title VARCHAR(255),
+	title_id VARCHAR(10) NOT NULL,
+	title VARCHAR(35),
 	PRIMARY KEY (title_id)
 	);
 	
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
-	emp_title_id VARCHAR(255) NOT NULL,
+	emp_title_id VARCHAR(35) NOT NULL,
 	birth_date DATE,
-	first_name VARCHAR(255),
-	last_name VARCHAR(255),
-	sex VARCHAR(255),
+	first_name VARCHAR(35),
+	last_name VARCHAR(35),
+	sex VARCHAR(35),
 	hire_date DATE,
 	PRIMARY KEY (emp_no),
 	FOREIGN KEY (emp_title_id) REFERENCES job_titles(title_id)		
 	);
 	
 CREATE TABLE departments (
-	dept_no VARCHAR(255) NOT NULL,
-	dept_name VARCHAR(255),
+	dept_no VARCHAR(10) NOT NULL,
+	dept_name VARCHAR(35),
 	PRIMARY KEY (dept_no)
 	);
 
 CREATE TABLE department_employees (
 	emp_no INT NOT NULL,
-	dept_no VARCHAR(255) NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 	);
 	
 CREATE TABLE department_manager (
-	dept_no VARCHAR(255) NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
 	emp_no INT NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)		
